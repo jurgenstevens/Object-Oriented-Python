207bb4804fd55498017e9ebd993242477e8c3eb6
Jurgen <jurgen.stevens@yahoo.com>
2020-08-28 01:34:36 -0500

    Added customer objects to a list and accessed value from the list.

--- python-oop.py
+++ python-oop.py
@@ -2,5 +2,17 @@ class Customer:
     def __init__(self, name, membership):
         self.name = name
         self.membership = membership
-    
-    
+
+# # create the customer like this
+# jurgen = Customer("Jurgen", "Premium")
+# print(jurgen.name, jurgen.membership)
+# # another example of creating a customer
+# caleb = Customer("Caleb", "Bronze")
+# print(caleb.name, caleb.membership)
+
+
+#  you can create a list of objects created with the class
+customers = [Customer("Jurgen", "Premium"), 
+            Customer("Caleb", "Bronze")]
+
+print(customers[1].name) # => Caleb
