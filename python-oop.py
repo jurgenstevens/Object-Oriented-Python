072f2736d156e8d4019183d6d0d3ec7517128e1c
Jurgen <jurgen.stevens@yahoo.com>
2020-08-28 23:43:58 -0500

    Created custom method to update Customer memberships.

--- python-oop.py
+++ python-oop.py
@@ -1,7 +1,13 @@
-class Customer:
+class Customer: # initialize the parameters below
+    # INIT method
     def __init__(self, name, membership):
         self.name = name
         self.membership = membership
+
+    def update_membership(self, new_membership):
+        self.membership = new_membership
+
+
 
 # # create the customer like this
 # jurgen = Customer("Jurgen", "Premium")
@@ -11,8 +17,17 @@ class Customer:
 # print(caleb.name, caleb.membership)
 
 
-#  you can create a list of objects created with the class
+#  you can create a list of objects created with the class. They're arguments
 customers = [Customer("Jurgen", "Premium"), 
             Customer("Caleb", "Bronze")]
 
 print(customers[1].name) # => Caleb
+
+# Custom Methods
+# customers[1].verified = False
+# print(customers[1].verified) # => False
+
+# Use Custom Method to update the membership
+print(customers[1].membership) # => Bronze
+customers[1].update_membership("Gold")
+print(customers[1].membership) # => Gold  
